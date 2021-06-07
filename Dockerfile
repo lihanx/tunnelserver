34abce513956e87bfe94e0f5a7429c06fb76c6bf
unknown <hanxuan.li@istarshine.com>
2021-06-07 10:09:50 +0800

    update

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.8
-ADD . /root/proxyserver
-WORKDIR /root/proxyserver
-RUN pip install w3lib uvloop redis -i https://pypi.douban.com/simple/
-CMD python main.py
--- src/main.py
+++
@@ -1,8 +0,0 @@
-# -*- coding:utf-8 -*-
-
-from server import ProxyServer
-import settings
-
-if __name__ == "__main__":
-    srv = ProxyServer()
-    srv.run("0.0.0.0", settings.PORT)
